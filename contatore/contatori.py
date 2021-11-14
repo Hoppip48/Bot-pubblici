b7c4f70f86fdc57cde8f9af8094199b15e945a5e
Hoppip48 <63052345+Hoppip48@users.noreply.github.com>
2021-11-14 10:35:58 +0100

    Update contatori.py

--- contatore/contatori.py
+++ contatore/contatori.py
@@ -1,10 +1,9 @@
 from datetime import date, time
 
-INIZIO = date(2021,9,10)
-FINESCUOLA = date(2022,6,21)
-PENNY = date(2022,6,6)
-LUCA = date(2021,11,22)
-GIANCA = date(2021,11,24)
-ZYERCAS = date(2022,2,5)
-SIMEME = date(2022,4,9)
-ABE = date(2022,2,14)
+#yyyy/mm/gg
+
+INIZIO = date(2021,9,10) #inserisci data inizio scuola
+FINESCUOLA = date(2022,6,21) #inserisci data fine scuola 
+COMPLEANNO = date(2001,1,1) #inserisci tuo compleanno
+IDGRUPPO = 0 #inserisci id del gruppo/canale/tag persona
+DIALOGHI = ["","",""] #Puoi inserire infiniti buongiorni qui e verranno scelti randomicamente]
