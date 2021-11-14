43ee5f64e9f486b6764fa850b2be1d60167c5c44
Hoppip48 <63052345+Hoppip48@users.noreply.github.com>
2021-11-14 10:36:50 +0100

    Update contatori.py

--- contatore/contatori.py
+++ contatore/contatori.py
@@ -7,3 +7,5 @@ FINESCUOLA = date(2022,6,21) #inserisci data fine scuola
 COMPLEANNO = date(2001,1,1) #inserisci tuo compleanno
 IDGRUPPO = 0 #inserisci id del gruppo/canale/tag persona
 DIALOGHI = ["","",""] #Puoi inserire infiniti buongiorni qui e verranno scelti randomicamente]
+API_ID = 0 #inserisci il tuo telegram api id
+HASH_ID = 0 #inserisci il tuo telegram hash id
