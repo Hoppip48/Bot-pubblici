483beaf621c30fbdc7aa0fc393a1681ee0440e9b
Hoppip48 <63052345+Hoppip48@users.noreply.github.com>
2021-11-14 10:29:01 +0100

    Add files via upload

---
+++ contatore/contatori.py
@@ -0,0 +1,10 @@
+from datetime import date, time
+
+INIZIO = date(2021,9,10)
+FINESCUOLA = date(2022,6,21)
+PENNY = date(2022,6,6)
+LUCA = date(2021,11,22)
+GIANCA = date(2021,11,24)
+ZYERCAS = date(2022,2,5)
+SIMEME = date(2022,4,9)
+ABE = date(2022,2,14)
